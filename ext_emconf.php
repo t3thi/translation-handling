193f92ba7e8c5f328dd96b311cad0a2219c79fa8
André Buchmann <andre.buchmann@twt.de>
2023-11-10 15:39:02 +0100

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,16 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'translation_handling',
+    'description' => 'TYPO3 extension to showcase TYPO3 translation handling capabilities',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.0.0-12.4.99',
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'T3thi\\TranslationHandling\\' => 'Classes/',
+        ],
+    ],
+];
