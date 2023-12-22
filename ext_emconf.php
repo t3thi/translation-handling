16785007c76d23909a609d4d1896590c9cbd6b48
André Buchmann <andre.buchmann@twt.de>
2023-12-22 14:59:58 +0100

    Add fluid_styled_content to requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,11 +1,12 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'translation_handling',
+    'title' => 'TYPO3 Translation Handling',
     'description' => 'TYPO3 extension to showcase TYPO3 translation handling capabilities',
     'constraints' => [
         'depends' => [
             'typo3' => '12.0.0-13.99.99',
+            'fluid_styled_content' => '12.0.0-13.99.99',
         ],
     ],
     'autoload' => [
