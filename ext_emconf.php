4fef6cfa13d2bc3be2321053e9a46c0e5e9bebae
Eric Harrer <info@eric-harrer.de>
2025-11-07 13:24:01 +0100

    [TASK] Adjust compatibility to TYPO3 13.4 and 14.x dev
    Drop support for TYPO3 v12 and update composer.json and ext_emconf.php to declare compatibility with TYPO3 13.4 LTS and the 14.x development branch.

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,8 +5,8 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'TYPO3 extension to showcase TYPO3 translation handling capabilities',
     'constraints' => [
         'depends' => [
-            'typo3' => '14.0.0-14.99.99',
-            'fluid_styled_content' => '14.0.0-14.99.99',
+            'typo3' => '13.4.0-14.99.99',
+            'fluid_styled_content' => '13.4.0-14.99.99',
         ],
     ],
     'autoload' => [
